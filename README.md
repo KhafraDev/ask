4845c766ab15f326e900ad02bf0f65862bf67943
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-12-26 14:58:12 -0500

    v1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ask
  prompt users in CLI
+
+# Example
+```js
+const ask = require('ask');
+
+ask('How old are you? ', {
+    validate: i => !isNaN(i) && i >= 18
+}).then(console.log);
+```
--- ask.js
+++ ask.js
@@ -1,18 +1,39 @@
 const { createInterface } = require('readline');
 const { promisify } = require('util');
 
-const readline = createInterface({ 
-    input: process.stdin, 
-    output: process.stdout 
-});
-
-readline.question[promisify.custom] = q => new Promise(resolve => readline.question(q, resolve));
-
 /**
  * Answer a question
  * @param {string} p Question to ask.
+ * @param {Object} o Options
+ * @param {Function} [o.validate] Validation function.
  * @returns {Promise<string>} Answer to the question. 
  */
-const ask = p => promisify(readline.question)(p);
+const ask = async (p, o = {}) => {
+    const readline = createInterface({ 
+        input: process.stdin, 
+        output: process.stdout 
+    });
+    
+    readline.question[promisify.custom] = q => new Promise(resolve => readline.question(q, resolve));
+    const question = promisify(readline.question);
+
+    while(true) {
+        const answer = await question(p);
+        if(o.validate instanceof Function) {
+            const value = o.validate(answer);
+
+            if(value === true) {
+                readline.close();
+                return answer;
+            } else {
+                process.stdout.moveCursor(0, -1);  // move one line up
+                process.stdout.cursorTo(p.length); // move cursor to end of question
+                process.stdout.clearLine();  // clear the text.
+            }
+        } else {
+            return answer;
+        }
+    }
+}
 
 module.exports = ask;
